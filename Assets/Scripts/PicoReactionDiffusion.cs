633ae50d74c9b26aa6ddec5d9e47270dfa58a435
Dan Lewis <daniel.lewis.software.engineer@gmail.com>
2019-02-19 11:45:37 -0600

    Reaction Diffusion compute shader

--- Assets/Scripts/PicoReactionDiffusion.cs
+++ Assets/Scripts/PicoReactionDiffusion.cs
@@ -22,17 +22,27 @@ public class PicoReactionDiffusion : MonoBehaviour
     float centerWeight = -1;
     float adjacentWeight = 0.2f;
     float diagonalWeight = 0.05f;
+    [Range(0, 100)]
+    public int T = 0;
     // Cell[,] front;
     // Cell[,] back;
     ComputeBuffer frontBuffA;
     ComputeBuffer frontBuffB;
     ComputeBuffer backBuffA;
     ComputeBuffer backBuffB;
+    // ComputeBuffer colorsBuff;
+    RenderTexture Screen;
+    // Texture Colors;
     float[] arrA;
     float[] arrB;
+    public Color colorA;
+    public Color colorB;
+    // int[] colorsArr;
+    public uint pow;
     int w;
     int h;
     Pico p;
+    Camera m_MainCamera;
 
     int kernel;
     // Start is called before the first frame update
@@ -45,6 +55,8 @@ public class PicoReactionDiffusion : MonoBehaviour
         compute.SetFloat("Db", Db);
         compute.SetFloat("f", f);
         compute.SetFloat("k", k);
+        compute.SetVector("colorA", new Vector4(colorA.r, colorA.g, colorA.b, colorA.a));
+        compute.SetVector("colorB", new Vector4(colorB.r, colorB.g, colorB.b, colorB.a));
         compute.SetFloat("centerWeight", centerWeight);
         compute.SetFloat("adjacentWeight", adjacentWeight);
         compute.SetFloat("diagonalWeight", diagonalWeight);
@@ -52,32 +64,47 @@ public class PicoReactionDiffusion : MonoBehaviour
         compute.SetBuffer(kernel, "SourceB", SourceB);
         compute.SetBuffer(kernel, "DestA", DestA);
         compute.SetBuffer(kernel, "DestB", DestB);
+
+        // compute.SetTexture(kernel, "Colors", Colors);
+        // compute.SetBuffer(kernel, "Colors", colorsBuff);
+
         compute.Dispatch(kernel, w / 8, h / 8, 1);
     }
 
     void Start()
     {
-        p = GetComponent<Pico>();
-        w = (int)p.Width;
-        h = (int)p.Height;
+        // p = GetComponent<Pico>();
+        m_MainCamera = Camera.main;
+        var dim = (int)Mathf.Pow(2, pow);
+        w = dim;
+        h = dim;
         kernel = compute.FindKernel("CSMain");
+
 
         frontBuffA = new ComputeBuffer(w * h, sizeof(float));
         frontBuffB = new ComputeBuffer(w * h, sizeof(float));
         backBuffA = new ComputeBuffer(w * h, sizeof(float));
         backBuffB = new ComputeBuffer(w * h, sizeof(float));
+        Screen = new RenderTexture(w, h, 24);
+        Screen.enableRandomWrite = true;
+        Screen.Create();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = Screen;
+        // Colors = new Texture2D(w,h,TextureFormat.RGBA);
+        // colorsBuff = new ComputeBuffer(w * h, sizeof(int));
         arrA = new float[w * h];
         arrB = new float[w * h];
+        // colorsArr = new int[w * h];
 
         // front = new Cell[p.Width, p.Height];
         // back = new Cell[p.Width, p.Height];
-
+        var v = new Vector2(w / 2, h / 2);
         for (int x = 0; x < w; x++)
         {
             for (int y = 0; y < h; y++)
             {
                 arrA[(y * w) + x] = 1.0f;
-                if (Random.value < 0.1f)
+                if (x > (w / 2 - 10) && x < (w / 2 + 10) && y > (h / 2 - 5) && y < (h / 2 + 5))
                 {
                     arrB[(y * w) + x] = 1.0f;
                 }
@@ -91,6 +118,7 @@ public class PicoReactionDiffusion : MonoBehaviour
         backBuffA.SetData(arrA);
         frontBuffB.SetData(arrB);
         backBuffB.SetData(arrB);
+        compute.SetTexture(kernel, "Screen", Screen);
     }
 
     // float convolveA(int x, int y, ref Cell[,] arr)
@@ -145,6 +173,21 @@ public class PicoReactionDiffusion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2? mousePosition = null;
+        if (m_MainCamera)
+        {
+            RaycastHit hit;
+            Ray ray = m_MainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit) && hit.transform == this.transform)
+            {
+                var m = Vector2.Scale(hit.textureCoord, new Vector2(w - 1, (h - 1)));
+                mousePosition = m;
+            }
+            else
+            {
+                mousePosition = null;
+            }
+        }
         var t = 1;
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -163,110 +206,141 @@ public class PicoReactionDiffusion : MonoBehaviour
         }
 
 
-        if (p.mousePosition.HasValue && (Input.GetMouseButton(0) || Input.GetMouseButton(1)))
+        if (mousePosition.HasValue && (Input.GetMouseButton(0) || Input.GetMouseButton(1)))
         {
-            var m = p.mousePosition.Value;
+            var m = mousePosition.Value;
+            var x = (int)m.x;
+            var y = (int)m.y;
             if (useBack)
             {
                 backBuffB.GetData(arrB);
-                arrB[Mathf.FloorToInt(m.x) + (Mathf.FloorToInt(m.y) * w)] = Input.GetMouseButton(0) ? 1 : 0;
+                for (int _x = (x - 1) < 0 ? 0 : (x - 1); _x <= x + 1 && _x < w; _x++)
+                {
+                    for (int _y = (y - 1) < 0 ? 0 : (y - 1); _y <= y + 1 && _y < h; _y++)
+                    {
+                        arrB[_x + (y * w)] = Input.GetMouseButton(0) ? 1 : 0;
+                    }
+                }
+                // arrB[Mathf.FloorToInt(m.x) + (Mathf.FloorToInt(m.y) * w)] = Input.GetMouseButton(0) ? 1 : 0;
                 backBuffB.SetData(arrB);
             }
             else
             {
                 frontBuffB.GetData(arrB);
-                arrB[Mathf.FloorToInt(m.x) + (Mathf.FloorToInt(m.y) * w)] = Input.GetMouseButton(0) ? 1 : 0;
+                for (int _x = (x - 1) < 0 ? 0 : (x - 1); _x <= x + 1 && _x < w; _x++)
+                {
+                    for (int _y = (y - 1) < 0 ? 0 : (y - 1); _y <= y + 1 && _y < h; _y++)
+                    {
+                        arrB[_x + (y * w)] = Input.GetMouseButton(0) ? 1 : 0;
+                    }
+                }
                 frontBuffB.SetData(arrB);
             }
         }
-
+        for (int i = 0; i < T; i++)
+        {
+            if (useBack)
+            {
+                runShader(backBuffA, backBuffB, frontBuffA, frontBuffB);
+            }
+            else
+            {
+                runShader(frontBuffA, frontBuffB, backBuffA, backBuffB);
+            }
+            useBack = !useBack;
+        }
         if (useBack)
         {
             runShader(backBuffA, backBuffB, frontBuffA, frontBuffB);
-            frontBuffA.GetData(arrA);
-            frontBuffB.GetData(arrB);
+            // frontBuffA.GetData(arrA);
+            // frontBuffB.GetData(arrB);
+
         }
         else
         {
             runShader(frontBuffA, frontBuffB, backBuffA, backBuffB);
-            backBuffA.GetData(arrA);
-            backBuffB.GetData(arrB);
-        }
-
-
-        for (int x = 0; x < w; x++)
-        {
-            for (int y = 0; y < h; y++)
-            {
-                // float A;
-                // float B;
-                // if (useBack)
-                // {
-                //     A = back[x, y].A;
-                //     B = back[x, y].B;
-                //     A += ((Da * convolveA(x, y, ref back)) - (A * B * B) + (f * (1 - A))) * t;
-                //     B += ((Db * convolveB(x, y, ref back)) + (A * B * B) - ((k + f) * B)) * t;
-                //     if (A < 0)
-                //     {
-                //         A = 0;
-                //     }
-                //     else if (A > 1)
-                //     {
-                //         A = 1;
-                //     }
-                //     if (B < 0)
-                //     {
-                //         B = 0;
-                //     }
-                //     else if (B > 1)
-                //     {
-                //         B = 1;
-                //     }
-                //     front[x, y].A = A;
-                //     front[x, y].B = B;
-                // }
-                // else
-                // {
-                //     A = front[x, y].A;
-                //     B = front[x, y].B;
-                //     A += ((Da * convolveA(x, y, ref front)) - (A * B * B) + (f * (1 - A))) * t;
-                //     B += ((Db * convolveB(x, y, ref front)) + (A * B * B) - ((k + f) * B)) * t;
-                //     if (A < 0)
-                //     {
-                //         A = 0;
-                //     }
-                //     else if (A > 1)
-                //     {
-                //         A = 1;
-                //     }
-                //     if (B < 0)
-                //     {
-                //         B = 0;
-                //     }
-                //     else if (B > 1)
-                //     {
-                //         B = 1;
-                //     }
-                //     back[x, y].A = A;
-                //     back[x, y].B = B;
-                // }
-                // var c = Mathf.FloorToInt((A / (A + B)) * 15);
-                // if (Random.value < 0.0001)
-                // {
-                //     print(A + " " + B + " " + (A / (A + B)) + " " + c);
-
-                // }
-                // if (Random.value > 0.2f) {
-                //     continue;
-                // }
-                var a = arrA[(y * w) + x];
-                var b = arrB[(y * w) + x];
-                var c = (int)((a / (a + b)) * 15);
-                p.pset(x, y, c);
-            }
+            // backBuffA.GetData(arrA);
+            // backBuffB.GetData(arrB);
         }
         useBack = !useBack;
-        p.flip();
+
+        // colorsBuff.GetData(colorsArr);
+
+
+        // for (int x = 0; x < w; x += 1)
+        // {
+        //     for (int y = 0; y < h; y += 1)
+        //     {
+        //         // float A;
+        //         // float B;
+        //         // if (useBack)
+        //         // {
+        //         //     A = back[x, y].A;
+        //         //     B = back[x, y].B;
+        //         //     A += ((Da * convolveA(x, y, ref back)) - (A * B * B) + (f * (1 - A))) * t;
+        //         //     B += ((Db * convolveB(x, y, ref back)) + (A * B * B) - ((k + f) * B)) * t;
+        //         //     if (A < 0)
+        //         //     {
+        //         //         A = 0;
+        //         //     }
+        //         //     else if (A > 1)
+        //         //     {
+        //         //         A = 1;
+        //         //     }
+        //         //     if (B < 0)
+        //         //     {
+        //         //         B = 0;
+        //         //     }
+        //         //     else if (B > 1)
+        //         //     {
+        //         //         B = 1;
+        //         //     }
+        //         //     front[x, y].A = A;
+        //         //     front[x, y].B = B;
+        //         // }
+        //         // else
+        //         // {
+        //         //     A = front[x, y].A;
+        //         //     B = front[x, y].B;
+        //         //     A += ((Da * convolveA(x, y, ref front)) - (A * B * B) + (f * (1 - A))) * t;
+        //         //     B += ((Db * convolveB(x, y, ref front)) + (A * B * B) - ((k + f) * B)) * t;
+        //         //     if (A < 0)
+        //         //     {
+        //         //         A = 0;
+        //         //     }
+        //         //     else if (A > 1)
+        //         //     {
+        //         //         A = 1;
+        //         //     }
+        //         //     if (B < 0)
+        //         //     {
+        //         //         B = 0;
+        //         //     }
+        //         //     else if (B > 1)
+        //         //     {
+        //         //         B = 1;
+        //         //     }
+        //         //     back[x, y].A = A;
+        //         //     back[x, y].B = B;
+        //         // }
+        //         // var c = Mathf.FloorToInt((A / (A + B)) * 15);
+        //         // if (Random.value < 0.0001)
+        //         // {
+        //         //     print(A + " " + B + " " + (A / (A + B)) + " " + c);
+
+        //         // }
+        //         if (Random.value > p.chance)
+        //         {
+        //             continue;
+        //         }
+        //         var a = arrA[(y * w) + x];
+        //         var b = arrB[(y * w) + x];
+        //         var c = (int)((a / (a + b)) * 15);
+        //         p.pset(x, y, c);
+        //     }
+        // }
+
+        // p.flip();
     }
 
     void OnDestroy()
