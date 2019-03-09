0ee987bcc4a265f70c9782ba32f5d3d51af1e590
Dan Lewis <daniel.lewis.software.engineer@gmail.com>
2019-03-09 00:46:25 -0600

    update videolab, SphereECS

---
+++ Assets/Scripts/SphereECS/SphereData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Entities;
+namespace SphereECS
+{
+    [Serializable]
+    public struct SphereData : IComponentData
+    {
+        public int x;
+        public int y;
+    }
+
+}
---
+++ Assets/Scripts/SphereECS/SphereSystem.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+
+namespace SphereECS
+{
+    public class SphereSystem : JobComponentSystem
+    {
+
+        struct SphereScaleJob : IJobProcessComponentData<Scale, SphereData>
+        {
+            public float time;
+            public int width;
+            public int height;
+            public Vector2 center;
+            public void Execute(ref Scale scale, [ReadOnly] ref SphereData sphereData)
+            {
+
+            }
+        }
+        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        {
+            var job = new SphereScaleJob()
+            {
+                time = Time.realtimeSinceStartup,
+                width = 100,
+                height = 100,
+                center = new Vector2Int(50, 50)
+            };
+
+            return job.Schedule(this, inputDependencies);
+        }
+    }
+
+}
+
